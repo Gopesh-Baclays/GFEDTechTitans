34b57ba3dbfd782268bc8aa93d835b17a19a057e
D Grewal <bh01182721@devcorptenant.com>
2025-06-04 12:50:17 +0530

    Hackathon Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RECAP.Models;
+
+namespace RECAP.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View("SystemLogin");
+    }
+
+    public IActionResult SSOLogin()
+    {
+        string userid = Environment.UserName;
+        if (string.IsNullOrEmpty(userid))
+        {
+            return RedirectToAction("SystemLogin");
+        }
+        else
+        {
+            return RedirectToAction("Dashboard");
+        }
+    }
+    public IActionResult Dashboard()
+    {
+        return View();
+    }
+     public IActionResult SystemLogin()
+    {
+        return View();
+    }
+ public IActionResult Data()
+    {
+        return View();
+    }
+     public IActionResult Rules()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RECAP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
