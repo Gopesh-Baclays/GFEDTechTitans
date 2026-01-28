11440ba77c2fd773aaff1410a4e7203dd84bd938
GGupta <gopesh.gupta2@barclays.com>
2026-01-28 23:20:05 +0530

    Commit

---
+++ Models/ProspectViewDetailModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace RECAP.Models
+{
+    public class ProspectDetailsViewModel
+    {
+        // The prospect selected by id (can be null)
+        public ProspectViewModel SelectedProspect { get; set; }
+
+        // 1-based index of the selected prospect (null when not provided)
+        public int? SelectedIndex { get; set; }
+
+        // Full list to render below
+        public List<ProspectViewModel> Prospects { get; set; } = new List<ProspectViewModel>();
+    }
+}
